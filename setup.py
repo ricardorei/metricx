8d6ff773a596ac638fed7c30005ade2b5a057e4a
Ricardo Rei <ricardo.rei@unbabel.com>
2024-06-18 16:23:22 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+install_requires = [
+    "transformers[torch]==4.30.2",
+    "sentencepiece==0.1.99",
+    "datasets",
+]
+
+setuptools.setup(
+    name="metricx",
+    version="0.1",
+    author="Daniel Deutsch",
+    description="Machine Translation Evaluation metric.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/google-research/metricx",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+    ],
+    license="Apache 2.0",
+    python_requires=">=3",
+    install_requires=install_requires,
+    dependency_links=[
+        'https://github.com/google-research/mt-metrics-eval'
+    ],
+)
